6ff23a2c7e583b5ce00faf0743e0450c6a77aa16
Kyle Amyx <kyle.amyx@duethealth.com>
2022-06-20 19:13:32 -0400

    removed application block- maybe this will fix deployment issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,15 +29,15 @@ plugins {
 group = "com.shadowconnect"
 version = "0.0.1"
 
-application {
-    mainClass.set("com.shadowconnect.ApplicationKt")
-
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
-}
+//application {
+//    mainClass.set("com.shadowconnect.ApplicationKt")
+//
+//    val isDevelopment: Boolean = project.ext.has("development")
+//    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+//}
 
 tasks {
-    create("stage").dependsOn("installDist")
+    create("stage").dependsOn("build", "installDist")
 }
 
 dependencies {
