388d3db6a56e0bdc1fa0be739550f7ec3574de51
Kyle Amyx <kyle.amyx@duethealth.com>
2022-06-20 19:00:48 -0400

    updated mainclass in application config

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ group = "com.shadowconnect"
 version = "0.0.1"
 
 application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
+    mainClass.set("com.shadowconnect.ApplicationKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
