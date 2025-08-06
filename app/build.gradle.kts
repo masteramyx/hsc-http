a2e368251e68ec364115e2bbfc28d54efee0cf54
Kyle Amyx <kyle@iheartjane.com>
2025-08-05 20:59:09 -0400

    configure CORS; not needed yet but for future-proofing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation(libs.ktor.server.netty)
     implementation(libs.ktor.server.sessions)
     implementation(libs.ktor.server.auth)
+    implementation(libs.ktor.server.cors)
     implementation(libs.ktor.server.html.builder)
     implementation(libs.bcrypt)
     implementation(libs.logback.classic)
--- app/src/main/kotlin/com/shadowconnect/Application.kt
+++ app/src/main/kotlin/com/shadowconnect/Application.kt
@@ -21,6 +21,7 @@ fun Application.module() {
     
     configureSessions()
     configureAuthentication()
+    configureCORS()
     configureRouting()
     configureSerialization()
     
